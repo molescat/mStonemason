bb45fbbab10a1e48ac55a50447ac7bf686ff7eaa
Myles Abbott <myles.abbott@rea-group.com>
2013-09-30 18:14:51 +1000

    Add reveal basics

--- mStonemason/MStoneAppDelegate.m
+++ mStonemason/MStoneAppDelegate.m
@@ -1,8 +1,4 @@
 //
-//  MStoneAppDelegate.m
-//  mStonemason
-//
-//  Created by Myles Abbott on 30/09/13.
 //  Copyright (c) 2013 Myles Abbott. All rights reserved.
 //
 
@@ -12,35 +8,36 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+  [self startReveal];
+  return YES;
 }
 							
-- (void)applicationWillResignActive:(UIApplication *)application
+#pragma mark - Reveal
+
+#import <dlfcn.h>
+- (void)startReveal
 {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  NSString *revealLibName = @"libReveal";
+  NSString *revealLibExtension = @"dylib";
+  NSString *dyLibPath = [[NSBundle mainBundle] pathForResource:revealLibName ofType:revealLibExtension];
+  NSLog(@"Loading dynamic library: %@", dyLibPath);
+  
+  void *revealLib = NULL;
+  revealLib = dlopen([dyLibPath cStringUsingEncoding:NSUTF8StringEncoding], RTLD_NOW);
+  
+  if (revealLib == NULL)
+  {
+    char *error = dlerror();
+    NSLog(@"dlopen error: %s", error);
+    NSString *message = [NSString stringWithFormat:@"%@.%@ failed to load with error: %s", revealLibName, revealLibExtension, error];
+    [[[UIAlertView alloc] initWithTitle:@"Reveal library could not be loaded" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+  }
+  else
+  {
+    // Post a notification to signal Reveal to start the service.
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"IBARevealRequestStart" object:self];
+  }
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
